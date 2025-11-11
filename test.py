c1333238b9ba49df3935d4472f4cdd144ef52ca9
NOTONE <94484201+notonecz@users.noreply.github.com>
2025-11-11 14:59:40 +0100

    Refactor if statements to elif for clarity

--- test.py
+++ test.py
@@ -5,17 +5,17 @@ den = int(input("Zadejte pořadové číslo dne: "))
 if 1 <= den <= 7:
     if den == 1:
         print("Pondělí")
-    if den == 2:
+    elif den == 2:
         print("Úterý")
-    if den == 3:
+    elif den == 3:
         print("Středa")
-    if den == 4:
+    elif den == 4:
         print("Čtvrtek")
-    if den == 5:
+    elif den == 5:
         print("Pátek")
-    if den == 6:
+    elif den == 6:
         print("Sobota")
-    if den == 7:
+    elif den == 7:
         print("Neděle")
 else:
     print("Neplatný den")
@@ -30,39 +30,39 @@ if 1 <= mesic <= 12:
 
         if mesic == 3:
             print("Březen")
-        if mesic == 4:
+        elif mesic == 4:
             print("Duben")
-        if mesic == 5:
+        elif mesic == 5:
             print("Květen")
 
         print("Jarní měsíc")
-    if 6 <= mesic <= 8:
+    elif 6 <= mesic <= 8:
 
         if mesic == 6:
             print("Červen")
-        if mesic == 7:
+        elif mesic == 7:
             print("Červenec")
-        if mesic == 8:
+        elif mesic == 8:
             print("Srpen")
 
         print("Letní měsíc")
-    if 9 <= mesic <= 11:
+    elif 9 <= mesic <= 11:
 
         if mesic == 9:
             print("Září")
-        if mesic == 10:
+        elif mesic == 10:
             print("Říjen")
-        if mesic == 11:
+        elif mesic == 11:
             print("Listopad")
 
         print("Podzminí měsíc")
-    if 1 <= mesic <= 2 or mesic == 12:
+    elif 1 <= mesic <= 2 or mesic == 12:
 
         if mesic == 1:
             print("Leden")
-        if mesic == 2:
+        elif mesic == 2:
             print("Únor")
-        if mesic == 12:
+        elif mesic == 12:
             print("Prosinec")
 
         print("Zimní měsíc")
